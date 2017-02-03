8cb4ff72929a9f3968fa0659fdff2b2fc83d0264
juju4 <juju4@users.noreply.github.com>
2017-02-02 20:01:57 -0500

    change mdl domain to test and check if present in config

--- test/integration/default/serverspec/mdl_spec.rb
+++ test/integration/default/serverspec/mdl_spec.rb
@@ -3,11 +3,16 @@ require 'serverspec'
 # Required by serverspec
 set :backend, :exec
 
-describe command('host mw077.ru') do
-#  its(:stdout) { should match /has address 92.53.114.87/ }
+describe file('/etc/bind/spywaredomains.zones') do
+  its(:content) { should match /Please visit www.malwaredomains.com for more information and to report/ }
+  its(:content) { should match /{type master; file "\/etc\/bind\/blockeddomain.hosts";};/ }
+  its(:content) { should match /surico.ru/ }
+end
+
+describe command('host surico.ru') do
   its(:stdout) { should match /has address 10.0.0.1/ }
 end
-describe command('host www.mw077.ru') do
+describe command('host www.surico.ru') do
   its(:stdout) { should match /has address 10.0.0.1/ }
 end
 
