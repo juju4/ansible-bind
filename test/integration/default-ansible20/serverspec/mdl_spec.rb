c165f30809741b8ccac59149279dc39b6eac4197
juju4 <juju4@users.noreply.github.com>
2017-02-16 21:08:33 -0500

    revert - trusty travis still stalling

--- test/integration/default-ansible20/serverspec/mdl_spec.rb
+++ test/integration/default-ansible20/serverspec/mdl_spec.rb
@@ -3,11 +3,12 @@ require 'serverspec'
 # Required by serverspec
 set :backend, :exec
 
-describe command('host mw077.ru') do
+## must reflect current mdl spywaredomains...
+describe command('host bakuzbuq.ru') do
 #  its(:stdout) { should match /has address 92.53.114.87/ }
   its(:stdout) { should match /has address 10.0.0.1/ }
 end
-describe command('host www.mw077.ru') do
+describe command('host www.bakuzbuq.ru') do
   its(:stdout) { should match /has address 10.0.0.1/ }
 end
 
--- test/integration/default/serverspec/mdl_spec.rb
+++ test/integration/default/serverspec/mdl_spec.rb
@@ -3,16 +3,22 @@ require 'serverspec'
 # Required by serverspec
 set :backend, :exec
 
-describe file('/etc/bind/spywaredomains.zones') do
+describe file('/etc/bind/spywaredomains.zones'), :if => os[:family] == 'ubuntu' || os[:family] == 'debian' do
+  its(:content) { should match /Please visit www.malwaredomains.com for more information and to report/ }
+  its(:content) { should match /{type master; file "\/etc\/bind\/blockeddomain.hosts";};/ }
+  its(:content) { should match /surico.ru/ }
+end
+describe file('/var/named/chroot/etc/spywaredomains.zones'), :if => os[:family] == 'redhat' do
   its(:content) { should match /Please visit www.malwaredomains.com for more information and to report/ }
   its(:content) { should match /{type master; file "\/etc\/bind\/blockeddomain.hosts";};/ }
   its(:content) { should match /surico.ru/ }
 end
 
-describe command('host surico.ru') do
+## must reflect current mdl spywaredomains...
+describe command('host bakuzbuq.ru') do
   its(:stdout) { should match /has address 10.0.0.1/ }
 end
-describe command('host www.surico.ru') do
+describe command('host www.bakuzbuq.ru') do
   its(:stdout) { should match /has address 10.0.0.1/ }
 end
 
