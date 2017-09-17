7d4f531495ff12b6cb5adf6a117534126a10539c
juju4 <juju4@users.noreply.github.com>
2017-09-17 18:09:53 -0400

    mdl serverspec test: no more surico

--- test/integration/default/serverspec/mdl_spec.rb
+++ test/integration/default/serverspec/mdl_spec.rb
@@ -6,12 +6,12 @@ set :backend, :exec
 describe file('/etc/bind/spywaredomains.zones'), :if => os[:family] == 'ubuntu' || os[:family] == 'debian' do
   its(:content) { should match /Please visit www.malwaredomains.com for more information and to report/ }
   its(:content) { should match /{type master; file "\/etc\/bind\/blockeddomain.hosts";};/ }
-  its(:content) { should match /surico.ru/ }
+  its(:content) { should match /bakuzbuq.ru/ }
 end
 describe file('/var/named/chroot/etc/spywaredomains.zones'), :if => os[:family] == 'redhat' do
   its(:content) { should match /Please visit www.malwaredomains.com for more information and to report/ }
   its(:content) { should match /{type master; file "\/var\/named\/chroot\/etc\/blockeddomain.hosts";};/ }
-  its(:content) { should match /surico.ru/ }
+  its(:content) { should match /bakuzbuq.ru/ }
 end
 
 ## must reflect current mdl spywaredomains...
